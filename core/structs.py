376f222e57b4e66ea0268ab5266bcfb093e2759d
Vladimir Margaryan <v.margaryan204@gmail.com>
2026-01-19 08:50:18 -0800

    feat: add power, ReLU and unbroadcasting functionality to Tensor class feat: add graph visualizer utility function for easier debugging

--- core/structs.py
+++ core/structs.py
@@ -2,57 +2,67 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, data, _children=(), requires_grad = False):
-        self.data = np.array(data)
-        self.grad = np.zeros_like(self.data)
+    def __init__(self, data, _children=(), requires_grad = False, op = "", default_scalar_type = np.float32):
+        self._dst = default_scalar_type
+        self.data = np.array(data, dtype=self._dst)
+        self.grad = np.zeros_like(self.data, dtype=self._dst)
         self._prev = set(_children)
         self._backward = lambda: None
         self.requires_grad = requires_grad
+        self._op = op
 
     @classmethod
-    def _get_validated_other_object(cls, other):
-        whitelist_instance_types = (
-            int,
-            float,
-            np.ndarray)
-
-        if isinstance(other, whitelist_instance_types):
-            return Tensor(other)
-        elif isinstance(other, Tensor):
+    def _get_validated_other_object(cls, other, default_type=np.float32):
+        if isinstance(other, cls):
             return other
-        else:
-            raise ValueError(f"The given operand {other} type is not acceptable!")
+
+        try:
+            data = np.array(other, dtype=default_type)
+            return cls(data)
+        except Exception as e:
+            raise TypeError(
+                f"Cannot implicitly convert {type(other)} to Tensor. "
+                f"Supported types: [Tensor, np.ndarray, int, float, list, tuple]. "
+                f"Error: {e}"
+            )
 
     def _unbroadcast(self, grad, shape):
-        ...
+        while grad.ndim > len(shape):
+            grad = grad.sum(axis=0)
+
+        for i, (g_dim, t_dim) in enumerate(zip(grad.shape, shape)):
+            if t_dim == 1 and g_dim > 1:
+                grad = grad.sum(axis=i, keepdims=True)
+
+        return grad
 
     def __repr__(self):
         return f"Tensor(data={self.data})"
 
     def __add__(self, other):
         other = self._get_validated_other_object(other)
-        out = Tensor(self.data + other.data, (self, other))
+        out = Tensor(self.data + other.data, (self, other), op="+")
         out.requires_grad = self.requires_grad or other.requires_grad
 
         def _backward():
             if self.requires_grad:
-                self.grad += out.grad
+                self.grad += self._unbroadcast(out.grad, self.data.shape)
             if other.requires_grad:
-                other.grad += out.grad
+                other.grad += self._unbroadcast(out.grad, other.data.shape)
 
         out._backward = _backward
         return out
 
     def __mul__(self, other):
         other = self._get_validated_other_object(other)
-        out = Tensor(self.data * other.data, (self, other))
+        out = Tensor(self.data * other.data, (self, other), op="*")
         out.requires_grad = self.requires_grad or other.requires_grad
 
         def _backward():
             if self.requires_grad:
-                self.grad += other.data * out.grad
+                self.grad += self._unbroadcast(other.data * out.grad, self.data.shape)
             if other.requires_grad:
-                other.grad += self.data * out.grad
+                other.grad += self._unbroadcast(self.data * out.grad, other.data.shape)
 
         out._backward = _backward
 
@@ -60,7 +70,7 @@ class Tensor:
 
     def __matmul__(self, other):
         other = self._get_validated_other_object(other)
-        out = Tensor(self.data @ other.data, (self, other))
+        out = Tensor(self.data @ other.data, (self, other), op="@")
         out.requires_grad = self.requires_grad or other.requires_grad
 
         def _backward():
@@ -74,7 +84,7 @@ class Tensor:
 
     def __rmatmul__(self, other):
         other = self._get_validated_other_object(other)
-        out = Tensor(other.data @ self.data, (self, other))
+        out = Tensor(other.data @ self.data, (self, other), op="@")
         out.requires_grad = self.requires_grad or other.requires_grad
 
         def _backward():
@@ -98,6 +108,19 @@ class Tensor:
     def __rsub__(self, other):
         return other + (-self)
 
+    def __pow__(self, power):
+        assert isinstance(power, (int, float)), "Currently only int/float are supported!"
+        out = Tensor(np.power(self.data, power), (self, ), op="^")
+        out.requires_grad = self.requires_grad
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += power * np.power(self.data, (power - 1)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
     def backward(self):
         topo = []
         visited = set()
@@ -113,4 +136,14 @@ class Tensor:
         for v in reversed(topo):
             v._backward()
 
-# TODO: Add broadcasting
+    def ReLU(self):
+        out = Tensor(np.maximum(0, self.data), (self,), op="ReLU")
+        out.requires_grad = self.requires_grad
+
+        def _backward():
+            if self.requires_grad:
+                self.grad += (self.data > 0) * out.grad
+
+        out._backward = _backward
+
+        return out
---
+++ tests/tensor_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestTensorOps(unittest.TestCase):
+    ...
