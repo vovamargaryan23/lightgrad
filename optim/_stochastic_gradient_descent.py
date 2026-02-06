74da62d0317e96d33d661f9b7350f0293e864691
Vladimir Margaryan <v.margaryan204@gmail.com>
2026-02-06 16:44:07 +0400

    add momentum to existing SGD

--- optim/_stochastic_gradient_descent.py
+++ optim/_stochastic_gradient_descent.py
@@ -1,11 +1,39 @@
+from typing import Sequence
+
+import numpy as np
+
+from nn import Parameter
 from optim import BaseOptimizer
 
 
 class StochasticGradientDescent(BaseOptimizer):
+    def __init__(self, parameters: Sequence[Parameter], lr: float, momentum: float | None = None):
+        super().__init__(parameters, lr)
+        if momentum is None:
+            self._step_function = self._step_through_default_sgd
+        else:
+            if not 0.0 <= momentum < 1.0:
+                raise ValueError("momentum must be in [0, 1)")
+            self.momentum = momentum
+            self._velocities = {id(p): np.zeros_like(p.data) for  p in self.parameters}
+            self._step_function = self._step_through_sgd_with_momentum
+
+    def _step_through_default_sgd(self, p):
+        grad = p.grad
+        p.data -= self.learning_rate * grad
+
+    def _step_through_sgd_with_momentum(self, p):
+        v = self._velocities[id(p)]
+
+        v = self.momentum * v + p.grad
+
+        self._velocities[id(p)] = v
+
+        p.data -= self.learning_rate * v
+
     def step(self):
         for p in self.parameters:
             if p.grad is None:
                 continue
 
-            grad = p.grad
-            p.data -= self.learning_rate * grad
+            self._step_function(p)
